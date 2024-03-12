75636d7bbefcbb542b686d4f8da580479483268d
Lluis Alsina Navarro <163167066+Kiske883@users.noreply.github.com>
2024-03-12 17:57:21 +0100

    Update MyUtils.java

--- MyUtils.java
+++ MyUtils.java
@@ -27,5 +27,50 @@ public class MyUtils {
             return null;
         }
     }
+  
+    public static String getCfgValue(String keyList, Properties cfgFileName, String defaultValue) {
 
+        String resultado = null;
+
+        try {
+
+            String[] keys = keyList.split("\t");
+
+            for (int i = 0; i < keys.length; i++) {
+
+                // log.info("Obteniendo el valor de la variable de entorno : " + keys[i]);
+                String myKey = keys[i];
+
+                resultado = System.getenv(keys[i]);
+
+                if (resultado == null) {
+                    // String myVariable = keys[i].replaceAll(".", "_");
+                    resultado = System.getenv(keys[i].replaceAll("\\.", "_"));
+                    // resultado = System.getenv(myVariable);
+                }
+
+                if (resultado == null) {
+                    log.debug("La variable de entorno : " + keys[i] + " no ha podido obtenerse. Obtenemos del fichero de configuración");
+                    resultado = cfgFileName.getProperty(keys[i], defaultValue);
+                }
+
+                if (resultado != null) {
+                    break;
+                }
+            }
+
+        } catch (Exception ex) {
+
+            log.error(ex.getMessage());
+
+        }
+
+
+        return resultado;
+    }
+
+    public static String getCfgValue(String keyList, Properties cfgFileName) {
+        return getCfgValue(keyList, cfgFileName, null);
+    }
+  
 }
